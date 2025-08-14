2b1de1a6886f3a5a5f3b0e0446913e138fd39b63
Petr Hrebenár <peta.hrebenar@gmail.com>
2025-08-14 08:54:03 +0000

    i want to use simple font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Literata', 'serif'],
-        headline: ['Literata', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
